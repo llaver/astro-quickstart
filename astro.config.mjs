fbe7238b86aa7d615ccb55e9487b73fb0a7a29a4
Ryan Bell <ryan.bell@nrg.com>
2026-03-05 14:37:01 -0600

    Update template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kpab.github.io',
+  base: '/astro-darkness',
+  trailingSlash: 'always',
+  integrations: [mdx(), sitemap()],
+});
